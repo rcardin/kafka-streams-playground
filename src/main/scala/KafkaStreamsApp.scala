aaef0cf05ede0f9e1a4b166a522dbb680350f0cc
rcardin <riccardo.cardin@gmail.com>
2021-09-05 15:38:41 +0200

    Added the source processor

--- src/main/scala/KafkaStreamsApp.scala
+++ src/main/scala/KafkaStreamsApp.scala
@@ -1,13 +1,23 @@
 package in.rcardin.kafka.streams
 
+import org.apache.kafka.streams.StreamsBuilder
+import org.apache.kafka.streams.kstream.KStream
+
 // Creation of the topic:
 // kafka-topics \
 //  --bootstrap-server localhost:9092 \
-//  --topic navigations \
+//  --topic navigation \
 //  --create
 object KafkaStreamsApp {
 
+  val NavigationTopic = "navigation"
+
   case class Navigation(user: String, page: String, millis: Long)
+
+  val builder = new StreamsBuilder
+
+  val source: KStream[String, Navigation] = builder.stream(NavigationTopic)
+
 
   def main(args: Array[String]): Unit = {
 
