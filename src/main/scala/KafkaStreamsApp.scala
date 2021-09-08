8e61ad4bf28f15cb74ca951a9beb0f236769d54f
rcardin <riccardo.cardin@gmail.com>
2021-09-08 22:20:56 +0200

    Changed the main example to a job scheduler

--- src/main/scala/KafkaStreamsApp.scala
+++ src/main/scala/KafkaStreamsApp.scala
@@ -1,23 +1,47 @@
 package in.rcardin.kafka.streams
 
+import org.apache.kafka.common.utils.Bytes
 import org.apache.kafka.streams.StreamsBuilder
-import org.apache.kafka.streams.kstream.KStream
+import org.apache.kafka.streams.kstream.{KStream, KTable, Materialized}
+import org.apache.kafka.streams.scala.serialization.Serdes
+import org.apache.kafka.streams.state.KeyValueStore
 
 // Creation of the topic:
 // kafka-topics \
 //  --bootstrap-server localhost:9092 \
-//  --topic navigation \
+//  --topic jobs \
 //  --create
+//
+// kafka-topics \
+// --bootstrap-server localhost:9092 \
+// --topic permissions \
+// --create
+// --config "cleanup.policy=compact"
 object KafkaStreamsApp {
 
-  val NavigationTopic = "navigation"
+  val Jobs: String = "jobs"
+  val Permissions: String = "permissions"
 
-  case class Navigation(user: String, page: String, millis: Long)
+  case class Job(user: String, params: Map[String, String])
+
+  case class AuthoredJob(job: Job, permissions: String)
 
   val builder = new StreamsBuilder
 
-  val source: KStream[String, Navigation] = builder.stream(NavigationTopic)
+  val source: KStream[String, Job] = builder.stream(Jobs)
 
+  val permissionsTable: KTable[String, String] = builder.table(
+    Permissions,
+    Materialized
+      .as[String, String, KeyValueStore[Bytes, Array[Byte]]]("permissions-store")
+      .withKeySerde(Serdes.stringSerde)
+      .withValueSerde(Serdes.stringSerde)
+  )
+
+  val authoredJobs: KStream[String, AuthoredJob] = source.join(
+    permissionsTable,
+    (job, permissions) => AuthoredJob(job, permissions)
+  )
 
   def main(args: Array[String]): Unit = {
 
