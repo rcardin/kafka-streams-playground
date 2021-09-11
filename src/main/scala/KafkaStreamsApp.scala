40336d3c17cce940400c291688ffda254b918928
rcardin <riccardo.cardin@gmail.com>
2021-09-11 15:43:10 +0200

    Created serde for the Job type

--- src/main/scala/KafkaStreamsApp.scala
+++ src/main/scala/KafkaStreamsApp.scala
@@ -1,10 +1,13 @@
 package in.rcardin.kafka.streams
 
-import org.apache.kafka.common.utils.Bytes
-import org.apache.kafka.streams.{KafkaStreams, StreamsBuilder, StreamsConfig, Topology}
-import org.apache.kafka.streams.kstream.{KStream, KTable, Materialized}
+import io.circe.generic.auto._
+import io.circe.parser._
+import io.circe.syntax._
+import org.apache.kafka.common.serialization.Serde
+import org.apache.kafka.streams.kstream.KStream
+import org.apache.kafka.streams.scala.kstream.Consumed
 import org.apache.kafka.streams.scala.serialization.Serdes
-import org.apache.kafka.streams.state.KeyValueStore
+import org.apache.kafka.streams.{KafkaStreams, StreamsBuilder, StreamsConfig, Topology}
 
 import java.util.Properties
 
@@ -30,36 +33,54 @@ import java.util.Properties
 //   --broker-list localhost:9092 \
 //   --property parse.key=true \
 //   --property key.separator=,
-//   user1,{"name": "user1", "name": "print", "params": {}}
+//   user1,{"user": "user1", "name": "print", "params": {}}
 object KafkaStreamsApp {
 
   val Jobs: String = "jobs"
   val Permissions: String = "permissions"
 
+  implicit val stringSerde: Serde[String] = Serdes.stringSerde
+
   case class Job(user: String, name: String, params: Map[String, String])
+  object Job {
+    implicit val jobSerde: Serde[Job] = Serdes.fromFn[Job](
+      (job: Job) => job.asJson.noSpaces.getBytes,
+      (jobBytes: Array[Byte]) => {
+        val stringJob = new String(jobBytes)
+        decode[Job](stringJob).toOption
+      }
+    )
+  }
 
   case class AuthoredJob(job: Job, permissions: String)
 
   val builder = new StreamsBuilder
 
-  val source: KStream[String, Job] = builder.stream(Jobs).peek { (user, job) => println(job) }
+  val source: KStream[String, Job] =
+    builder.stream(Jobs, Consumed.`with`[String, Job]).peek { (_, job) => println(job) }
 
-  val permissionsTable: KTable[String, String] = builder.table(
-    Permissions,
-    Materialized
-      .as[String, String, KeyValueStore[Bytes, Array[Byte]]]("permissions-store")
-      .withKeySerde(Serdes.stringSerde)
-      .withValueSerde(Serdes.stringSerde)
-  )
+  source.foreach((k, v) => println())
+//
+//  val permissionsTable: KTable[String, String] = builder.table(
+//    Permissions,
+//    Materialized
+//      .as[String, String, KeyValueStore[Bytes, Array[Byte]]]("permissions-store")
+//      .withKeySerde(Serdes.stringSerde)
+//      .withValueSerde(Serdes.stringSerde)
+//  )
+//
+//  val authoredJobs: KStream[String, AuthoredJob] = source.join(
+//    permissionsTable,
+//    (job, permissions) => AuthoredJob(job, permissions)
+//  )
+//
+//  authoredJobs.foreach { (user, authoredJob) =>
+//    println(s"The user $user, with roles ${authoredJob.permissions}, requested to execute job ${authoredJob.job.name}")
+//  }
 
-  val authoredJobs: KStream[String, AuthoredJob] = source.join(
-    permissionsTable,
-    (job, permissions) => AuthoredJob(job, permissions)
-  )
-
-  authoredJobs.foreach { (user, authoredJob) =>
-    println(s"The user $user, with roles ${authoredJob.permissions}, requested to execute job ${authoredJob.job.name}")
-  }
+//  builder
+//    .stream("jobs", Consumed.`with`(Serdes.stringSerde, Serdes.stringSerde))
+//    .foreach((k, v) => println(s"($k, $v)"))
 
   val topology: Topology = builder.build();
 
