f336e964771713fa7a29a433e34d0c5f44c34615
rcardin <riccardo.cardin@gmail.com>
2021-09-16 22:13:29 +0200

    Migrated to kafka streams types for Scala

--- src/main/scala/KafkaStreamsApp.scala
+++ src/main/scala/KafkaStreamsApp.scala
@@ -4,12 +4,12 @@ import io.circe.generic.auto._
 import io.circe.parser._
 import io.circe.syntax._
 import org.apache.kafka.common.serialization.Serde
-import org.apache.kafka.common.utils.Bytes
-import org.apache.kafka.streams.kstream.{KStream, KTable}
-import org.apache.kafka.streams.scala.kstream.{Consumed, Materialized}
+import org.apache.kafka.streams.scala.ImplicitConversions._
+import org.apache.kafka.streams.scala._
+import org.apache.kafka.streams.scala.kstream.{KStream, KTable}
 import org.apache.kafka.streams.scala.serialization.Serdes
-import org.apache.kafka.streams.state.KeyValueStore
-import org.apache.kafka.streams.{KafkaStreams, StreamsBuilder, StreamsConfig, Topology}
+import org.apache.kafka.streams.scala.serialization.Serdes._
+import org.apache.kafka.streams.{KafkaStreams, StreamsConfig, Topology}
 
 import java.util.Properties
 
@@ -41,17 +41,20 @@ object KafkaStreamsApp {
   val JobsTopic: String = "jobs"
   val PermissionsTopic: String = "permissions"
 
-  implicit val stringSerde: Serde[String] = Serdes.stringSerde
-
   // Can we implement a type class?
-
   case class Job(user: String, name: String, params: Map[String, String])
   object Job {
     implicit val jobSerde: Serde[Job] = {
       val serializer = (job: Job) => job.asJson.noSpaces.getBytes
       val deserializer = (jobAsBytes: Array[Byte]) => {
         val jobAsString = new String(jobAsBytes)
-        decode[Job](jobAsString).toOption
+        val jobOrError = decode[Job](jobAsString)
+        jobOrError match {
+          case Right(job) => Option(job)
+          case Left(error) =>
+            println(s"There was an error converting the message $jobOrError, $error")
+            Option.empty
+        }
       }
       Serdes.fromFn[Job](serializer, deserializer)
     }
@@ -63,8 +66,13 @@ object KafkaStreamsApp {
       val serializer = (permissions: Permissions) => permissions.asJson.noSpaces.getBytes
       val deserializer = (permissionsAsBytes: Array[Byte]) => {
         val permissionsAsString = new String(permissionsAsBytes)
-        val result = decode[Permissions](permissionsAsString)
-        result.toOption
+        val permissionsOrError = decode[Permissions](permissionsAsString)
+        permissionsOrError match {
+          case Right(permissions) => Option(permissions)
+          case Left(error) =>
+            println(s"There was an error converting the message $permissionsOrError, $error")
+            Option.empty
+        }
       }
       Serdes.fromFn[Permissions](serializer, deserializer)
     }
@@ -76,7 +84,13 @@ object KafkaStreamsApp {
       val serializer = (authoredJob: AuthoredJob) => authoredJob.asJson.noSpaces.getBytes
       val deserializer = (authoredJobAsBytes: Array[Byte]) => {
         val authoredJobAsString = new String(authoredJobAsBytes)
-        decode[AuthoredJob](authoredJobAsString).toOption
+        val authoredJobOrError = decode[AuthoredJob](authoredJobAsString)
+        authoredJobOrError match {
+          case Right(authoredJob) => Option(authoredJob)
+          case Left(error) =>
+            println(s"There was an error converting the message $authoredJobAsString, $error")
+            Option.empty
+        }
       }
       Serdes.fromFn[AuthoredJob](serializer, deserializer)
     }
@@ -85,17 +99,14 @@ object KafkaStreamsApp {
   val builder = new StreamsBuilder
 
   val source: KStream[String, Job] =
-    builder.stream(JobsTopic, Consumed.`with`[String, Job]).peek((k, v) => println(s"($k, $v)"))
+    builder.stream[String, Job](JobsTopic).peek((k, v) => println(s"($k, $v)"))
 
-  val permissionsTable: KTable[String, Permissions] = builder.table(
-    PermissionsTopic,
-    Materialized.`with`[String, Permissions, KeyValueStore[Bytes, Array[Byte]]]
-  )
+  val permissionsTable: KTable[String, Permissions] = builder.table(PermissionsTopic)
 
-  val authoredJobs: KStream[String, String] = source.join(
-    permissionsTable,
-    (job: Job, permissions: Permissions) => s"($job, $permissions)"
-  ).peek((k, v) => println(s"($k, $v)"))
+  val authoredJobs: KStream[String, AuthoredJob] =
+    source.join(permissionsTable) { (job: Job, permissions: Permissions) =>
+      AuthoredJob(job, permissions)
+  }
 
   authoredJobs.foreach { (user, authoredJob) =>
     println(s"The user $user was authored to job $authoredJob")
